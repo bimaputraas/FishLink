65a1ddfefa0b52693db3b86bf98618f32ec20c16
bimaxputra <bimaputrasejati9999@gmail.com>
2023-10-09 16:18:03 +0700

    Add skeleton folder for users and products

---
+++ api/dto/response.go
@@ -0,0 +1,25 @@
+package dto
+
+import "github.com/labstack/echo/v4"
+
+type Response struct {
+	Message string
+    Code int
+	Detail  interface{}
+}
+
+func WriteResponse(ctx echo.Context,code int, message string, detail interface{}) error {
+	return ctx.JSON(code,Response{
+        Message: message,
+        Code: code,
+        Detail: "-",
+    })
+}
+
+func WriteResponseWithDetail(ctx echo.Context,code int, message string, detail interface{}) error {
+	return ctx.JSON(code,Response{
+        Message: message,
+        Code: code,
+        Detail: detail,
+    })
+}
